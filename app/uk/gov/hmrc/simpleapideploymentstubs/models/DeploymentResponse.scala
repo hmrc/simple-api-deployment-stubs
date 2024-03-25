1fb0e470ccc4ae5a25a5e1198af3c07d984fff4d
mike.jackson1 <10079986+llmikeyj@users.noreply.github.com>
2024-03-25 16:34:11 +0000

    HIPP-1074: Add deployment/{id} stub

---
+++ app/uk/gov/hmrc/simpleapideploymentstubs/models/DeploymentResponse.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.simpleapideploymentstubs.models
+
+import play.api.libs.json.{Format, Json}
+
+case class DeploymentsResponse(id: String, version: String, mergeRequestIid: Int, uri: String)
+
+object DeploymentsResponse {
+
+  def apply(metadata: Metadata): DeploymentsResponse = {
+    DeploymentsResponse("test-id", "1.0.0", 102, "http://example.com/version/1.0.0")
+  }
+
+  implicit val formatDeploymentsResponse: Format[DeploymentsResponse] = Json.format[DeploymentsResponse]
+
+}
